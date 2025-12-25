85bf07e7a94edd217189dab4db879fa6bfd2caba
Best <bestsakditat@gmail.com>
2025-12-25 16:14:01 +0700

    chore: nitialize project and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-brand-primary text-headline-1">
+        My First React App on Vite
+      </h1>
+    </>
+  );
+}
+
+export default App;
