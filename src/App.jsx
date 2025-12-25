9d5da6b27065180040ca93dc87053713755c0db0
Best <bestsakditat@gmail.com>
2025-12-25 17:43:28 +0700

    chore: installing shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-brand-primary text-headline-1">
+      <h1 className="text-brand-primary text-headline-1 ">
         My First React App on Vite
       </h1>
     </>
